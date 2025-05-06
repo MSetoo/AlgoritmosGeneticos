7a8532b0012d6500334b5d19a5519722aa84ed30
MSetoo <dilan.calvache@epn.edu.ec>
2025-05-06 11:09:39 -0500

    si

---
+++ src/FuncionAptitud.java
@@ -0,0 +1,15 @@
+import org.jgap.FitnessFunction;
+import org.jgap.IChromosome;
+
+public class FuncionAptitud extends FitnessFunction {
+
+    @Override
+    //conjunto de cormososmas 
+    protected double evaluate(IChromosome ic) {
+        //crear la funcion 
+        //gen -> es una caracteristicas 
+        return 0; //siempre debe ser positivop
+    }
+
+    
+}
