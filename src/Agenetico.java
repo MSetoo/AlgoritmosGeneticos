2c3a54874ea26933308df698cf35a6ecdbae3a88
55Kevin-calapi <kcalapi84@gmail.com>
2025-05-08 10:28:47 -0500

    agentesgeneticosfinally

--- src/Agenetico.java
+++ src/Agenetico.java
@@ -2,12 +2,14 @@ import org.jgap.Chromosome;
 import org.jgap.Configuration;
 import org.jgap.Gene;
 import org.jgap.Genotype;
+import org.jgap.IChromosome;
 import org.jgap.InvalidConfigurationException;
 import org.jgap.impl.DefaultConfiguration;
 import org.jgap.impl.IntegerGene;
 public class Agenetico  {
     //cromosomas es un arreglo de genes
-    public void configurar(int genes) {
+    public void configurar(int genes, int poblacionInicial,int iteracionesGeneracion,
+    int evolucion) {
 
         Configuration config = new  DefaultConfiguration();  
         FuncionAptitud fa = new FuncionAptitud();  
@@ -25,7 +27,7 @@ public class Agenetico  {
        
     
     }
-    public void conf_poblacion(Configuration config, int poblacionInicial){
+    private void conf_poblacion(Configuration config, int poblacionInicial,int iteracionGenerecion , int evolucion){
 
         try {
             config.setPopulationSize(poblacionInicial);
@@ -35,5 +37,49 @@ public class Agenetico  {
             e.printStackTrace();
         }
             
+    }
+    public void evolucionar(Genotype poblacion, int iteracionGenerecion , int evolucion ){ 
+        ver_poblacion(poblacion.getChromosomes());
+        for (int i = 0; i < iteracionGenerecion ; i++ ){
+            poblacion.evolve(evolucion);
+           ver_individuo(poblacion.getFittestChromosome()); //obtener al mejhor individuo
+        }
+       System.out.println("Optimo: ");
+       ver_individuo(poblacion.getFittestChromosome());
+    }
+
+    private void ver_poblacion  (IChromosome[] ic){
+     System.out.println("----------------Poblacion----------");
+     for (IChromosome iChromosome : ic){
+        ver_individuo(iChromosome);
+     }
+     System.out.println("----------------------------");
+    }
+    private void ver_individuo (IChromosome ic){
+
+        System.out.println("**********individuo*********");
+        int signoX = (int)ic.getGene(0).getAllele();
+        int x1 = (int)ic.getGene(1).getAllele();
+        int x2 = (int)ic.getGene(2).getAllele();
+        int x3 = (int)ic.getGene(3).getAllele();
+        int x4 = (int)ic.getGene(4).getAllele();
+
+        int signoY = (int)ic.getGene(5).getAllele();
+        int y1 = (int)ic.getGene(6).getAllele();
+        int y2 = (int)ic.getGene(7).getAllele();
+        int y3 = (int)ic.getGene(8).getAllele();
+        int y4 = (int)ic.getGene(9).getAllele();
+        
+        int x_valor = Integer.parseInt ("x1"+"x2"+"x3"+"x4");
+        int y_valor = Integer.parseInt ("y1"+"y2"+"y3"+"y4");
+
+
+        if (signoX == 0 ){
+            x_valor = -x_valor;
+        }
+        if (signoY == 0 ){
+            y_valor = -y_valor;
+        }
+        System.out.println("");
     }
 }
--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      new Agenetico().configurar(10,4 ,5,6);
     }
 }
